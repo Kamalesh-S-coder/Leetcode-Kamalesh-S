fd94aa16bda938cd3144b0633b38c32f78e3f22b
Kamalesh-S-coder <kamaleshs9669@gmail.com>
2026-08-11 20:57:41 +0530

    Create 181-EmployeesEarningMoreThanTheirManagers

---
+++ 181-EmployeesEarningMoreThanTheirManagers/181-EmployeesEarningMoreThanTheirManagers.sql
@@ -0,0 +1,5 @@
+-- Last updated: 11/08/2026, 20:57:38
+# Write your MySQL query statement below
+# Write your MySQL query statement below
+SELECT  e1.name AS Employee FROM Employee e1 Join Employee e2 on e1.managerId=e2.id
+WHERE e1.salary>e2.salary;
