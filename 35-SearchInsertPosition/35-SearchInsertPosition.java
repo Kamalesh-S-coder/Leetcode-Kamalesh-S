7457c04abac5efd31f6c3cc62dcb9200b9fc0e0e
Kamalesh-S-coder <kamaleshs9669@gmail.com>
2026-08-11 21:01:49 +0530

    Create 35-SearchInsertPosition

---
+++ 35-SearchInsertPosition/35-SearchInsertPosition.java
@@ -0,0 +1,13 @@
+// Last updated: 11/08/2026, 21:01:46
+class Solution {
+        public int searchInsert(int[] A, int target) {
+        int low = 0, high = A.length-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(A[mid] == target) return mid;
+            else if(A[mid] > target) high = mid-1;
+            else low = mid+1;
+        }
+        return low;
+    }
+}
