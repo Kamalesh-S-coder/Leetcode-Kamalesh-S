3cc49a663563a0a16e5c359b9bdd9b99ba5dc82e
Kamalesh-S-coder <kamaleshs9669@gmail.com>
2026-08-11 14:20:02 +0530

    Create 602-FriendRequestsIiWhoHasTheMostFriends

---
+++ 602-FriendRequestsIiWhoHasTheMostFriends/602-FriendRequestsIiWhoHasTheMostFriends.sql
@@ -0,0 +1,7 @@
+-- Last updated: 11/08/2026, 14:20:01
+# Write your MySQL query statement below
+# Write your MySQL query statement below
+with base as(select requester_id id from RequestAccepted
+union all
+select accepter_id id from RequestAccepted)
+select id, count(*) num  from base group by 1 order by 2 desc limit 1
