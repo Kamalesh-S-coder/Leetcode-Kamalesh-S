d3168b33850bbf80b21d7b44659a019cc41c1071
Kamalesh-S-coder <kamaleshs9669@gmail.com>
2026-08-11 20:58:07 +0530

    Create 169-MajorityElement

---
+++ 169-MajorityElement/169-MajorityElement.java
@@ -0,0 +1,18 @@
+// Last updated: 11/08/2026, 20:58:04
+class Solution {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer, Integer> hash = new HashMap<>();
+        int res = 0;
+        int majority = 0;
+
+        for (int n : nums) {
+            hash.put(n, 1 + hash.getOrDefault(n, 0));
+            if (hash.get(n) > majority) {
+                res = n;
+                majority = hash.get(n);
+            }
+        }
+
+        return res;        
+    }
+}
