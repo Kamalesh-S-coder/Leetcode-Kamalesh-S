908629e3b6278648b3e27aa5971ffff75827b066
Kamalesh-S-coder <kamaleshs9669@gmail.com>
2026-08-11 14:16:23 +0530

    Create 3226-MinimumNumberGame

---
+++ 3226-MinimumNumberGame/3226-MinimumNumberGame.java
@@ -0,0 +1,34 @@
+// Last updated: 11/08/2026, 14:16:23
+class Solution {
+    public int[] numberGame(int[] nums) {
+
+        int[] count = new int[101];
+        int[] rez = new int[nums.length];
+        int idx = 0;
+
+        for (int num : nums) {
+            count[num]++;
+        }
+
+        for (int num = 1; num < 101; num++) {
+
+            while (count[num] > 0) {
+
+                int first = num;
+                count[num]--;
+
+                while (count[num] == 0) {
+                    num++;
+                }
+
+                int second = num;
+                count[num]--;
+
+                rez[idx++] = second;
+                rez[idx++] = first;
+            }
+        }
+
+        return rez;
+    }
+}
