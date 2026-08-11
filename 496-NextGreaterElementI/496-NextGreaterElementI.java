c40182f4c878afb5e40b6a7171df63ce8fb19aa0
Kamalesh-S-coder <kamaleshs9669@gmail.com>
2026-08-11 14:20:38 +0530

    Create 496-NextGreaterElementI

---
+++ 496-NextGreaterElementI/496-NextGreaterElementI.java
@@ -0,0 +1,27 @@
+// Last updated: 11/08/2026, 14:20:38
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> m = new HashMap<>();
+        Stack<Integer> st= new Stack<>();
+        for(int ele: nums2){
+            if(!st.isEmpty() && ele>st.peek()){
+                // Nested loop added here to process all smaller elements 
+                // while keeping your exact outer 'if' structure intact
+                while(!st.isEmpty() && ele > st.peek()) {
+                    m.put(st.pop(),ele);
+                }
+            }
+            st.push(ele);
+        }
+        int[] res = new int[nums1.length];
+        for(int i=0;i<nums1.length;i++){
+            if(m.containsKey(nums1[i])){
+                res[i] = m.get(nums1[i]);
+            }
+            else{
+                res[i] = -1;
+            }
+        }
+        return res;
+    }
+}
