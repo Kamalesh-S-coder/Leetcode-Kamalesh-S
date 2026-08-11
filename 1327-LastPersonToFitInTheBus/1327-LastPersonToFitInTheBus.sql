a69d5b78fc03f1e4646911055630397f870f368c
Kamalesh-S-coder <kamaleshs9669@gmail.com>
2026-08-11 14:17:42 +0530

    Create 1327-LastPersonToFitInTheBus

---
+++ 1327-LastPersonToFitInTheBus/1327-LastPersonToFitInTheBus.sql
@@ -0,0 +1,10 @@
+-- Last updated: 11/08/2026, 14:17:42
+# Write your MySQL query statement below
+# Write your MySQL query statement below
+SELECT person_name
+FROM (SELECT person_name, turn,
+    SUM(weight) OVER (ORDER BY turn) AS cum_wei
+    FROM Queue
+) temp 
+WHERE cum_wei <= 1000
+ORDER BY turn DESC LIMIT 1;
