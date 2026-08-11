c83dd5e090cb25d01490b932fcff6154495ac502
Kamalesh-S-coder <kamaleshs9669@gmail.com>
2026-08-11 20:55:28 +0530

    Create 284-PeekingIterator

---
+++ 284-PeekingIterator/284-PeekingIterator.java
@@ -0,0 +1,34 @@
+// Last updated: 11/08/2026, 20:55:25
+class PeekingIterator implements Iterator<Integer> {
+    
+    Iterator<Integer> iter = null;
+    Integer next = null;
+    
+	public PeekingIterator(Iterator<Integer> iterator) {
+	    // initialize any member here.
+	    iter = iterator;
+        if(hasNext()) next = iter.next();
+	}
+	
+    // Returns the next element in the iteration without advancing the iterator.
+	public Integer peek() {
+        return next;
+	}
+	
+	// hasNext() and next() should behave the same as in the Iterator interface.
+	// Override them if needed.
+	@Override
+	public Integer next() {
+	    Integer currVal = next;
+        if(iter.hasNext()) 
+            next = iter.next();
+        else 
+            next = null;
+        return currVal;
+	}
+	
+	@Override
+	public boolean hasNext() {
+	    return next != null || iter.hasNext();
+	}
+}
