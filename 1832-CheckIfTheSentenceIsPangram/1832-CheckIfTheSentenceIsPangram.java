b084475ee9be9762f205713138babdf392776897
Kamalesh-S-coder <kamaleshs9669@gmail.com>
2026-09-11 09:26:24 +0530

    Create 1832-CheckIfTheSentenceIsPangram

---
+++ 1832-CheckIfTheSentenceIsPangram/1832-CheckIfTheSentenceIsPangram.java
@@ -0,0 +1,12 @@
+// Last updated: 11/09/2026, 09:26:24
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        if(sentence.length()<26) return false;
+        for(char ch='a';ch<='z';ch++){
+            if(sentence.indexOf(ch)<0){
+                return false;
+            }
+         }
+        return true;
+    }
+}
