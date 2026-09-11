721322cbe1c2506ccc08418d284a93bb2380fb04
Kamalesh-S-coder <kamaleshs9669@gmail.com>
2026-09-11 09:31:57 +0530

    Create 553-OptimalDivision

---
+++ 553-OptimalDivision/553-OptimalDivision.java
@@ -0,0 +1,16 @@
+// Last updated: 11/09/2026, 09:31:56
+class Solution {
+    public String optimalDivision(int[] nums) {
+        int n = nums.length;
+        StringBuilder sb = new StringBuilder().append(nums[0]);
+
+        if (n == 1) return sb.toString();
+        if (n == 2) return sb.append("/").append(nums[1]).toString();
+        sb.append("/(");
+        for (int i = 1; i < n; i++) 
+            sb.append(nums[i]).append("/");
+        
+        sb.setCharAt(sb.length()-1, ')');
+        return sb.toString();
+    }
+}
