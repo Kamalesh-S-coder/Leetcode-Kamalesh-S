db3c34f6d91386d240f6b7986dcab11ff3bc38a2
Kamalesh-S-coder <kamaleshs9669@gmail.com>
2026-09-11 09:30:09 +0530

    Create 645-SetMismatch

---
+++ 645-SetMismatch/645-SetMismatch.java
@@ -0,0 +1,28 @@
+// Last updated: 11/09/2026, 09:30:09
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+
+        int n = nums.length;
+        int duplicate = 0;
+        int missing = 0;
+        for (int i = 1; i <= n; i++) {
+            int count = 0;
+
+            for (int j = 0; j < n; j++) {
+                if (nums[j] == i) {
+                    count++;
+                }
+            }
+
+            if (count == 2) {
+                duplicate = i;
+            }
+
+            if (count == 0) {
+                missing = i;
+            }
+        }
+
+        return new int[]{duplicate, missing};
+    }
+}
